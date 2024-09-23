3fbf525ecd5bf59915aaa6f45dde9056209b6a2f
Paul Oyelabi <oyelabipaul@gmail.com>
2024-09-23 17:21:40 +0100

    chore: initial commit
        -   added models for database tables.

---
+++ internal/models/user.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"path"
+	"time"
+)
+
+type User struct {
+	ID          string
+	FirstName   string
+	LastName    string
+	Email       string
+	PhoneNum    string
+	Bio         string
+	Avatar      string
+	Role        string
+	ServiceRole string
+	IsVerified  bool
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// AvatarPath returns the relative path to user's avatar.
+func (u *User) AvatarPath() string {
+	return path.Join("profiles/avatar", u.ID)
+}
