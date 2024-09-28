0557caec89bafba07969bec617ff0eaeb9438698
Paul Oyelabi <oyelabipaul@gmail.com>
2024-09-29 00:36:52 +0100

    chore: updated user database model struct
        -   added fields to show contact verification.
        -   addded model for verification code.

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,20 +6,30 @@ import (
 )
 
 type User struct {
-	ID          string
-	FirstName   string
-	LastName    string
-	Email       string
-	PhoneNum    string
-	Password    string
-	Bio         string
-	Avatar      string
-	Role        string
-	ServiceRole string
-	IsVerified  bool
+	ID            string
+	FirstName     string
+	LastName      string
+	Email         string
+	PhoneNum      string
+	Password      string
+	Bio           string
+	Avatar        string
+	Role          string
+	ServiceRole   string
+	IsVerified    bool
+	EmailVerified bool
+	PhoneVerified bool
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+type UserVerificationCode struct {
+	UserID      string
+	Code        int
+	ContactType string
+	CreatedAt   time.Time
+	ExpiresAt   time.Time
 }
 
 // AvatarPath returns the relative path to user's avatar.
