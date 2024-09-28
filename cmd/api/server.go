687d80f4b3a7d9ffd8fed57114355cc327d76a74
Paul Oyelabi <oyelabipaul@gmail.com>
2024-09-29 00:44:49 +0100

    chore: added mail and SMS sending utility to app
        -   added handlers for sending mail and SMS to application struct.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -8,9 +8,11 @@ import (
 
 	"github.com/lokatalent/backend_go/cmd/api/util"
 	"github.com/lokatalent/backend_go/internal/database/postgres"
+	"github.com/lokatalent/backend_go/internal/mailer"
 	"github.com/lokatalent/backend_go/internal/repository"
 	"github.com/lokatalent/backend_go/internal/s3"
 	"github.com/lokatalent/backend_go/internal/server/routes"
+	"github.com/lokatalent/backend_go/internal/sms"
 )
 
 func serveApp(config *util.Config, db *sql.DB) error {
@@ -22,6 +24,20 @@ func serveApp(config *util.Config, db *sql.DB) error {
 	app := util.Application{
 		Config:       config,
 		Repositories: repos,
+		Mailer: mailer.New(
+			mailer.Credentials{
+				APIKey: config.SendGrid.APIKey,
+				Sender: config.SendGrid.Sender,
+			},
+		),
+		SMSSender: sms.New(
+			sms.Credentials{
+				APIKey:     config.Twilio.APIKey,
+				APISecret:  config.Twilio.APISecret,
+				AccountSID: config.Twilio.AccountSID,
+				Sender:     config.Twilio.Sender,
+			},
+		),
 	}
 
 	engine := routes.Engine(&app)
