9f8efc8f36bbe438b47fd6f02cd05df948d0a255
Paul Oyelabi <oyelabipaul@gmail.com>
2024-10-16 11:14:42 +0100

    chore: updated database constraints constants
        -   added constants for duplicate `email, service and bank account number`

--- internal/database/postgres/constants.go
+++ internal/database/postgres/constants.go
@@ -8,6 +8,8 @@ const DB_QUERY_TIMEOUT = 15 * time.Second
 
 // users table constraints
 const (
-	duplicateEmail = "users_email_key"
-	duplicatePhone = "unique_nonempty_phone_num"
+	duplicateEmail       = "unique_nonempty_email"
+	duplicatePhone       = "unique_nonempty_phone_num"
+	duplicateService     = "unique_user_id_service_type"
+	duplicateBankAcctNum = "users_bank_info_account_num_key"
 )
