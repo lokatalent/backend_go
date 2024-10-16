5f1c0dd91724dc6ddb3e8e7d78ebd85c9b6834ff
Paul Oyelabi <oyelabipaul@gmail.com>
2024-10-16 11:27:17 +0100

    chore: updated constants
        -   added new constants for `contact verification`,
            `service roles`, and `bookings`.

--- internal/models/constants.go
+++ internal/models/constants.go
@@ -7,11 +7,35 @@ const (
 	USER_ADMIN_SUPER = "admin_super"
 )
 
+const (
+	EMAIL_VERIFICATION = "email"
+	PHONE_VERIFICATION = "phone"
+)
+
 // user service roles
 const (
 	SERVICE_PROVIDER  = "service_provider"
 	SERVICE_REQUESTER = "service_requester"
 	SERVICE_BOTH      = "service_both"
+)
+
+// user service types
+const (
+	SERVICE_CLEANING = "cleaning"
+	SERVICE_PLUMBING = "plumbing"
+	SERVICE_COOKING  = "cooking"
+)
+
+// bookings
+const (
+	// booking status
+	BOOKING_OPEN        = "open"
+	BOOKING_COMPLETED   = "completed"
+	BOOKING_IN_PROGRESS = "in_progress"
+
+	// booking type
+	BOOKING_INSTANT   = "instant"
+	BOOKING_SCHEDULED = "scheduled"
 )
 
 const (
