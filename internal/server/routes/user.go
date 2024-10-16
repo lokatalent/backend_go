91154cabe2365ddcaf645428c50b7e798669b901
Paul Oyelabi <oyelabipaul@gmail.com>
2024-10-16 11:35:37 +0100

    chore: updated users routes
        -   updated endpoint names
        -   added new routes for `education profile`, `bank profile`,
            `services`, `certifications`.

--- internal/server/routes/user.go
+++ internal/server/routes/user.go
@@ -24,13 +24,77 @@ func setUserRoutes(app *util.Application, engine *echo.Echo) {
 	// profile
 	user.GET("/profile", handler.GetOwnProfile, middleware.Authentication(app))
 	user.GET(
+		"/profile/education",
+		handler.GetOwnEducationProfile, middleware.Authentication(app))
+	user.GET(
+		"/profile/bank",
+		handler.GetOwnBankProfile, middleware.Authentication(app))
+	user.GET(
 		"/:id/profile", handler.GetProfile, middleware.PublicAuthentication(app))
+	user.GET(
+		"/:id/profile/education",
+		handler.GetEducationProfile, middleware.PublicAuthentication(app))
 	user.PATCH(
-		"/profile/update", handler.UpdateProfile, middleware.Authentication(app))
+		"/profile",
+		handler.UpdateProfilePersonal,
+		middleware.Authentication(app),
+	)
+	user.PATCH(
+		"/profile/education",
+		handler.UpdateProfileEducation,
+		middleware.Authentication(app),
+	)
+	user.PATCH(
+		"/profile/bank",
+		handler.UpdateProfileBank,
+		middleware.Authentication(app),
+	)
 	user.PATCH(
 		"/profile/picture-update", handler.UpdateProfileImage,
 		middleware.Authentication(app))
 	user.DELETE(
 		"/profile/picture-delete", handler.DeleteProfileImage,
 		middleware.Authentication(app), middleware.RequireVerification)
+
+	user.GET(
+		"/:id/profile/certifications",
+		handler.GetCertifications,
+		middleware.Authentication(app),
+	)
+	user.POST(
+		"/profile/certifications",
+		handler.UploadCertifications,
+		middleware.Authentication(app),
+	)
+	user.DELETE(
+		"/profile/certifications/:id",
+		handler.DeleteCertification,
+		middleware.Authentication(app),
+	)
+
+	// services
+	user.GET("/:id/service", handler.GetService, middleware.Authentication(app))
+	user.GET("/:id/service/list", handler.ListServices, middleware.Authentication(app))
+	user.POST(
+		"/service", handler.CreateService, middleware.Authentication(app))
+	user.PATCH(
+		"/service", handler.UpdateService, middleware.Authentication(app))
+	user.DELETE(
+		"/service", handler.DeleteService, middleware.Authentication(app))
+
+	user.GET(
+		"/:id/service/images",
+		handler.GetServiceImages,
+		middleware.Authentication(app),
+	)
+	user.POST(
+		"/service/images",
+		handler.UploadServiceImages,
+		middleware.Authentication(app),
+	)
+	user.DELETE(
+		"/service/images/:id",
+		handler.DeleteServiceImage,
+		middleware.Authentication(app),
+	)
 }
