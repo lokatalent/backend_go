d1bc93bf573845c8581059b666a0f18536a6c7ad
Paul Oyelabi <oyelabipaul@gmail.com>
2024-10-16 11:42:27 +0100

    chore: updated user database repository
        -   updated function declarations and arguments
        -   added functions for `education`, `bank`, and
            `services` database operations.

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -3,11 +3,12 @@ package repository
 import "github.com/lokatalent/backend_go/internal/models"
 
 type UserRepository interface {
-	Create(user models.User) (models.User, error)
+	Create(user *models.User) error
 	GetByID(id string) (models.User, error)
 	GetByEmail(email string) (models.User, error)
+	GetByPhone(phone string) (models.User, error)
 	GetAllUsers(filter models.Filter) ([]models.User, error)
-	Update(user models.User) (models.User, error)
+	Update(user *models.User) error
 	UpdateImage(id string, imageURL string) error
 	ChangeRole(id, role string) error
 	ChangeServiceRole(id, role string) error
@@ -15,7 +16,29 @@ type UserRepository interface {
 
 	Verify(id string, status bool) error
 	VerifyContact(id, verificationType string, status bool) error
-	CreateVerificationCode(id, verificationType string, code int) error
+	CreateVerificationCode(verCode *models.UserVerificationCode) error
 	DeleteVerificationCode(id, verificationType string) error
 	GetVerificationCode(id, verificationType string) (models.UserVerificationCode, error)
+
+	CreateBankInfo(bankInfo *models.UserBankInfo) error
+	GetBankInfo(userID string) (models.UserBankInfo, error)
+	UpdateBankInfo(bankInfo *models.UserBankInfo) error
+
+	CreateEducationInfo(eduInfo *models.UserEducationInfo) error
+	GetEducationInfo(userID string) (models.UserEducationInfo, error)
+	UpdateEducationInfo(eduInfo *models.UserEducationInfo) error
+
+	CreateCertification(cert *models.UserCertification) error
+	GetCertifications(userID string) ([]models.UserCertification, error)
+	DeleteCertification(id, userID string) error
+
+	CreateService(service *models.UserService) error
+	GetService(userID, serviceType string) (models.UserService, error)
+	GetAllServices(userID string) ([]models.UserService, error)
+	UpdateService(service *models.UserService) error
+	DeleteService(userID, serviceType string) error
+
+	CreateServiceImage(seviceImg *models.ServiceImage) error
+	GetServiceImages(userID, serviceType string) ([]models.ServiceImage, error)
+	DeleteServiceImage(id, userID, serviceType string) error
 }
