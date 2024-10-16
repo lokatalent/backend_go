34237c789b64216c6b942caff60db966f625f748
Paul Oyelabi <oyelabipaul@gmail.com>
2024-10-16 11:32:38 +0100

    chore: updated users model struct
        -   added new fields to user struct.
        -   added struct to model users' `education profile`,
            `bank profile`, `certifications`.
        -   updated `AvartarPath` function.

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,7 +12,10 @@ type User struct {
 	Email         string
 	PhoneNum      string
 	Password      string
+	Gender        string
+	DateOfBirth   time.Time
 	Bio           string
+	Address       string
 	Avatar        string
 	Role          string
 	ServiceRole   string
@@ -25,14 +28,45 @@ type User struct {
 }
 
 type UserVerificationCode struct {
-	UserID      string
-	Code        int
-	ContactType string
-	CreatedAt   time.Time
-	ExpiresAt   time.Time
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	Code        int       `json:"code"`
+	ContactType string    `json:"contact_type"`
+	CreatedAt   time.Time `json:"created_at"`
+	ExpiresAt   time.Time `json:"expires_at"`
+}
+
+type UserBankInfo struct {
+	UserID      string    `json:"user_id"`
+	BankName    string    `json:"bank_name"`
+	AccountName string    `json:"account_name"`
+	AccountNum  string    `json:"account_num"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type UserEducationInfo struct {
+	UserID     string    `json:"user_id"`
+	Institute  string    `json:"institute"`
+	Degree     string    `json:"degree"`
+	Discipline string    `json:"discipline"`
+	Start      time.Time `json:"start"`
+	Finish     time.Time `json:"finish"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type UserCertification struct {
+	ID     string `json:"id"`
+	UserID string `json:"user_id"`
+	URL    string `json:"url"`
 }
 
 // AvatarPath returns the relative path to user's avatar.
 func (u *User) AvatarPath() string {
-	return path.Join("profiles/avatar", u.ID)
+	return path.Join("profiles", "avatars", u.ID)
+}
+
+func (u *UserCertification) CertificationPath() string {
+	return path.Join("profiles", "certifications", u.UserID, u.ID)
 }
