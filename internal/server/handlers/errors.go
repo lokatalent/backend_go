f48388d90f649cda63400e22f8fbcf4cddc376d3
Paul Oyelabi <oyelabipaul@gmail.com>
2024-11-24 16:54:54 +0100

    chore: updated errors list.

--- internal/server/handlers/errors.go
+++ internal/server/handlers/errors.go
@@ -8,7 +8,7 @@ var (
 	ErrInvalidEmail            = errors.New("Invalid email address")
 	ErrInvalidPhone            = errors.New("Invalid phone number")
 	ErrAlreadyVerified         = errors.New("Already verified")
-	ErrVerificationDependency  = errors.New("email and phone number needs to be verified!")
+	ErrVerificationDependency  = errors.New("email or phone number needs to be verified!")
 	ErrExpiredVerificationCode = errors.New("verification code has expired.")
 	ErrInvalidVerificationCode = errors.New("invalid verification code.")
 	ErrInvalidServiceType      = errors.New("invalid service type.")
